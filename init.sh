1c475b6ee32e15fbc189413e284cf03d071161c0
Carlgo11 <carlgo11@users.noreply.github.com>
2023-01-08 23:04:11 +0100

    Make script compatible with ro systems

--- init.sh
+++ init.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 # Install dependencies
-apk add --no-cache curl git python3 py3-pip openssl tar bash && ln -sf python3 /usr/bin/python
+apk add --no-cache curl python3 py3-pip openssl tar bash && ln -sf python3 /usr/bin/python
 
 # Decompress dehydrated.tar.gz
 tar xf /tmp/dehydrated-*.tar.gz --strip=1 -C .
+ln -s "$(pwd)/dehydrated" /usr/local/bin/
 
-# Download CF hook
-git clone https://github.com/walcony/letsencrypt-cloudflare-hook hooks/cloudflare
+# Decompress hook
+unzip /tmp/master.zip -d /tmp/ && mkdir hooks && mv /tmp/letsencrypt-cloudflare-hook-master hooks/cloudflare
 pip3 install --ignore-installed -r hooks/cloudflare/requirements.txt
 
 # Prepare paths
-mkdir -p /tmp/certs/ /etc/dehydrated/archive
+mkdir -p /persistent
